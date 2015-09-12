673d589ac1d7ac8fc7377ae900fdef6df156947a
sebamora84 <sebamora@gmail.com>
2015-09-12 01:47:13 -0400

    action bar icons changed

--- app/build/generated/source/r/debug/com/mlstuff/apps/tcl/R.java
+++ app/build/generated/source/r/debug/com/mlstuff/apps/tcl/R.java
@@ -1528,8 +1528,11 @@ containing a value of this type.
         public static final int abc_textfield_search_material=0x7f02003a;
         public static final int arrownext=0x7f02003b;
         public static final int arrowprev=0x7f02003c;
-        public static final int notification_template_icon_bg=0x7f02003e;
-        public static final int shape=0x7f02003d;
+        public static final int ic_mlstuff=0x7f02003d;
+        public static final int ic_share_black=0x7f02003e;
+        public static final int ic_share_white=0x7f02003f;
+        public static final int notification_template_icon_bg=0x7f020041;
+        public static final int shape=0x7f020040;
     }
     public static final class id {
         public static final int action0=0x7f0c0053;
